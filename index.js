966b9e16843387aa39a4b1308c98e99b83c1a3a1
gustavo <gustavo_moure@hotmail.com>
2020-05-10 13:01:38 -0300

    agregue permiso acceso datos

--- index.js
+++ index.js
@@ -3,20 +3,37 @@ const express = require('express'),
     jwt = require('jsonwebtoken'),
     config = require('./configs/config'),
     app = express();
+
 // 1  configuracion de l a clave 
 app.set('llave', config.llave);
+
 // 2  seteo para que body parser convierta lo que viene del cliente
 app.use(bodyParser.urlencoded({ extended: true }));
+
 // 3 se lo pasa a JSON
 app.use(bodyParser.json());
 // 4   
 app.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000')
 });
+
+const rutasProtegidas = express.Router(); 
+
 // 5
+
 app.get('/', function (req, res) {
     res.send('Inicio');
 });
+
+app.get('/datos', rutasProtegidas, (req, res) => {
+    const datos = [
+     { id: 1, nombre: "Asfo" },
+     { id: 2, nombre: "Denisse" },
+     { id: 3, nombre: "Carlos" }
+    ];
+    
+    res.json(datos);
+   });
 
 
 app.post('/autenticar', (req, res) => {
@@ -57,4 +74,24 @@ function generateToken(user) {
     return token = jwt.sign(user, app.get('llave'), {
        expiresIn: 60 * 60 * 24 // expires in 24 hours
     })
-  }
+  };
+
+  
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+ 
+    if (token) {
+      jwt.verify(token, app.get('llave'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválida' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveída.' 
+      });
+    }
+ });
